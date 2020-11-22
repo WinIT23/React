3e785ac57a2bff84f69eadea5932cc80186a52dd
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-22 18:02:13 +0530

    implement conditional output

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,9 @@ import Ninjas from './Ninjas';
 class App extends Component {
   state = {
     ninjas: [
-      { name: "Vinit", age: 21, belt: "Black", id: 1 },
+      { name: "Vinit", age: 31, belt: "Black", id: 1 },
       { name: "Vinit2", age: 21, belt: "Brown", id: 2 },
-      { name: "Vinit3", age: 21, belt: "Blue", id: 3 }
+      { name: "Vinit3", age: 35, belt: "Blue", id: 3 }
     ]
   }
   render() {
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ ninjas }) => {
+
+  // -------------- using if condition ----------------
+  // const ninjaList = ninjas.map(ninja => {
+  //   if(ninja.age > 29) {
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: {ninja.name} </div>
+  //         <div>Age: {ninja.age} </div>
+  //         <div>Belt: {ninja.belt} </div>
+  //       </div>
+  //     )
+  //   } else {
+  //     return null 
+  //   }
+  // })
+
+  // -------------- using if condition ----------------
   const ninjaList = ninjas.map(ninja => {
-    return (
+    return ninja.age > 29 ? (
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name} </div>
         <div>Age: {ninja.age} </div>
         <div>Belt: {ninja.belt} </div>
       </div>
-    )
-  });
+    ) : null
+  })
   return (
     <div className="ninja-list">
       {ninjaList}
