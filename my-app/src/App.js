b8bd463d2ad60d9057c7289e3f84f4527076454a
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-22 18:58:09 +0530

    added option to add ninja

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
 import { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
+  id = 4;
   state = {
     ninjas: [
       { name: "Vinit", age: 31, belt: "Black", id: 1 },
@@ -9,12 +11,23 @@ class App extends Component {
       { name: "Vinit3", age: 35, belt: "Blue", id: 3 }
     ]
   }
+
+  addNinja = (ninja) => {
+    ninja.id = this.id;
+    this.id++;
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first react App</h1>
         <p>Welcome! :)</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
