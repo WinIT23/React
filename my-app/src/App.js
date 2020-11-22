87c9884ce8f15b1150ffdf64ac48d8f9091d5c54
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-22 17:14:39 +0530

    implement component list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,19 @@ import { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Vinit", age: 21, belt: "Black", id: 1 },
+      { name: "Vinit2", age: 21, belt: "Brown", id: 2 },
+      { name: "Vinit3", age: 21, belt: "Blue", id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react App</h1>
         <p>Welcome! :)</p>
-        <Ninjas name="Vinit 2" age="22" belt="Black" />
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
