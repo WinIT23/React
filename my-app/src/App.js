e294cfa1bd8a18bf9f4d502ec51048e386f940c9
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-22 17:00:42 +0530

    implement component and use props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,13 @@ import Ninjas from './Ninjas';
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      <h1>My first react App</h1>
-      <p>Welcome! :)</p>
-      <Ninjas name="Vinit 2" age="22" belt="Black" />
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>My first react App</h1>
+        <p>Welcome! :)</p>
+        <Ninjas name="Vinit 2" age="22" belt="Black" />
+      </div>
+    );
   }
 }
 
