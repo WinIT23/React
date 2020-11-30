26ac9894f56494afce099c8cd4a62a4b757c371b
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-30 17:15:37 +0530

    todoapp: added click to delete todo

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Eat Maggie'},
+      {id: 2, content: 'Play Valorent'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter( todo => {
+      return todo.id !== id
+    });
+
+    // key and value are same so below it's 'value' and not 'key: value'
+    this.setState({
+      todos
+    })
+  }
+  render() {
+    return(
+      <div className="todo-App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+}
 }
 
 export default App;
