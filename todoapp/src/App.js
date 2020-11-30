b32b33558802679c519499fcdc726a803d6a4baf
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-11-30 17:44:24 +0530

    todoapp: added form to add todo

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos.jsx';
+import AddTodo from './AddTodo'
 
 class App extends Component {
   state = {
@@ -9,20 +10,26 @@ class App extends Component {
     ]
   }
   deleteTodo = (id) => {
-    const todos = this.state.todos.filter( todo => {
-      return todo.id !== id
-    });
+    const todos = this.state.todos.filter(todo => todo.id !== id);
 
     // key and value are same so below it's 'value' and not 'key: value'
     this.setState({
       todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
   render() {
     return(
       <div className="todo-App container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
 }
