ea0ace587646fff73402cbd2ef4ee6ea12aadf7b
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-12-04 01:25:05 +0530

    poketime: added post details

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
 import Contact from './component/Contact';
+import Post from './component/Post';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path='/' component={ Home } />
           <Route path='/about' component={ About } />
           <Route path='/contact' component={ Contact } />
+          <Route path='/posts/:post_id' component={ Post } />
         </div>
       </BrowserRouter>
     );
